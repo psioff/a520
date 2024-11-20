05a4678639deae0ee6229eb001ae9ad8f0ffb989
psioff <psioff@users.noreply.github.com>
2024-11-20 15:18:59 +0300

    remove utilities to /usr/local/bin

--- update.sh
+++ update.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
+# fw update prepairing, after reboot you should bios enter and select "remote firmware update" to finish
 wget -O /boot/efi/Shell.efi https://github.com/psioff/a520/raw/refs/heads/main/Shell.efi
 wget -O /boot/efi/startup.nsh https://github.com/psioff/a520/raw/refs/heads/main/startup.nsh
 wget -O /boot/efi/startup.efi https://github.com/psioff/a520/raw/refs/heads/main/startup.efi
 wget -O /boot/efi/startup.bin https://github.com/psioff/a520/raw/refs/heads/main/startup.bin
-#
-mkdir bin
-cd bin
-wget https://github.com/psioff/a520/raw/refs/heads/main/cbs
-wget https://github.com/psioff/a520/raw/refs/heads/main/fan
-wget https://github.com/psioff/a520/raw/refs/heads/main/pbs
-wget https://github.com/psioff/a520/raw/refs/heads/main/rtc
-wget https://github.com/psioff/a520/raw/refs/heads/main/usb
-chmod +x *
-cd ..
+
+# variable related utils
+wget -O /usr/local/bin/cbs https://github.com/psioff/a520/raw/refs/heads/main/cbs
+wget -O /usr/local/bin/fan https://github.com/psioff/a520/raw/refs/heads/main/fan
+wget -O /usr/local/bin/pbs https://github.com/psioff/a520/raw/refs/heads/main/pbs
+wget -O /usr/local/bin/rtc https://github.com/psioff/a520/raw/refs/heads/main/rtc
+wget -O /usr/local/bin/usb https://github.com/psioff/a520/raw/refs/heads/main/usb
+chmod +x /usr/local/bin/*
 
 reboot
 
