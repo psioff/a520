e9f4bf43d03799536bfe7f80eda886795038fe27
psioff <psioff@users.noreply.github.com>
2024-11-20 14:35:42 +0300

    update fw and user utils at same time

--- update.sh
+++ update.sh
@@ -4,6 +4,16 @@ wget -O /boot/efi/Shell.efi https://github.com/psioff/a520/raw/refs/heads/main/S
 wget -O /boot/efi/startup.nsh https://github.com/psioff/a520/raw/refs/heads/main/startup.nsh
 wget -O /boot/efi/startup.efi https://github.com/psioff/a520/raw/refs/heads/main/startup.efi
 wget -O /boot/efi/startup.bin https://github.com/psioff/a520/raw/refs/heads/main/startup.bin
+#
+mkdir bin
+cd bin
+wget https://github.com/psioff/a520/raw/refs/heads/main/cbs
+wget https://github.com/psioff/a520/raw/refs/heads/main/fan
+wget https://github.com/psioff/a520/raw/refs/heads/main/pbs
+wget https://github.com/psioff/a520/raw/refs/heads/main/rtc
+wget https://github.com/psioff/a520/raw/refs/heads/main/usb
+chmod +x *
+cd ..
 
 reboot
 
